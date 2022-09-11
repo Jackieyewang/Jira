14ef45458546c6f7dfb88c059ad9abf733b97dd1
Jackieyewang <306981258@qq.com>
2022-09-11 15:39:38 +0800

    chore: 添加泛型,js转ts

---
+++ src/screens/project-lists/index.tsx
@@ -0,0 +1,42 @@
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { useState, useEffect } from "react";
+import qs from "qs";
+import { cleanObject, useDebounce, useMount } from "utils";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+  const debouncedParam = useDebounce(param, 300);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
